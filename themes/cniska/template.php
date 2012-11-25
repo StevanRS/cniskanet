65c6c1d10e66f58f71fb7483f08eb05c4ba1e3bd
Crisu83 <ChristofferNiska@gmail.com>
2012-11-26 01:58:28 +0200

    Change to use LESS instead of CSS

--- themes/cniska/template.php
+++ themes/cniska/template.php
@@ -1,14 +1,27 @@
 <?php
 
+use Drupal\Core\Template\Attribute;
+
 /**
  * @file
  * Functions to support theming in this theme.
  */
 
-function cniska_preprocess_html() {
-	drupal_add_html_head_link(array(
-		'href' => 'http://fonts.googleapis.com/css?family=Open+Sans',
-		'rel' => 'stylesheet',
-		'type' => 'text/css',
-	));
+function cniska_menu_link(array $variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
+
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }
+
+  switch($element['#theme']) {
+    // Set rel="nofollow" for the "follow me" menu items.
+    case 'menu_link__menu_follow':
+      $element['#attributes']['rel'] = 'nofollow';
+      break;
+  }
+
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . new Attribute($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
